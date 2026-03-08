464f6cac32e01fd74a9ab740b95c2fca29a67f6d
Paul Caton <paul_caton@hotmail.com>
2026-03-08 11:39:29 +0000

    Tweaked README and comments

--- I-Ching.cpp
+++ I-Ching.cpp
@@ -5,17 +5,17 @@ I Ching, also known as the Book of Changes, is an ancient Chinese divination tex
 It consists of 64 hexagrams, each made up of six lines that can be either broken (yin) or unbroken (yang). The hexagrams are used to represent different situations and their 
 potential outcomes, and they are often consulted for decision-making and understanding the flow of events.
 
-This program is a modern implementation of an I Ching divination tool that allows users to cast hexagrams using a hardware random number generator, the RDSEED CPU instruction. 
+This program is a modern implementation of an I Ching divination tool that allows users to cast hexagrams using a hardware-based random number generator, the RDSEED CPU instruction. 
 Providing high-quality random numbers from Johnson-Nyquist thermal noise.
 
-The program also includes features for saving and loading hexagram readings, as well as displaying the associated text and interpretations for each hexagram and its changing lines.
+The program also includes features for saving and loading saved readings, as well as displaying a modernised text for each hexagram and its changing lines.
 
-Gottfried Wilhelm Leibniz, the co-inventor of calculus with Newton, was inspired by the I Ching, particularly its binary system, which influenced his development of modern binary 
-code used in computing. He saw connections between the I Ching's hexagrams and the binary sequence, linking ancient Chinese philosophy to contemporary mathematics and technology.
+Leibniz, the co-inventor of calculus with Newton, was inspired by the I Ching, particularly its binary system, which influenced the development of modern binary 
+code used in computing. He saw connections between the I Ching's hexagrams and binary representation, linking ancient Chinese philosophy to contemporary mathematics and technology.
 
 https://therealsamizdat.com/2016/07/15/eco-the-i-ching-and-the-binary-calculus/
 
-In particular, see Figure 14.1 and the associated discussion. The binary sequence from thousands of years ago! 
+In particular, see Figure 14.1 and the associated discussion. The binary sequence from thousands of years ago!
 
 */
 
